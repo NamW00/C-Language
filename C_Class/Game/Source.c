9ee22d9844f8e37273d922bbd59e57bb69a3de6c
NamWoo <156279584+NamW00@users.noreply.github.com>
2024-01-12 21:49:59 +0900

    (2024.1.12) 파일 입출력

--- C_Class/Game/Source.c
+++ C_Class/Game/Source.c
@@ -1,106 +1,71 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <time.h>
 
-void UpdateHPBar(int life)
-{
-	for (int i = 0; i < life; i++) {
-		printf("♥");
-	}
+#define BUFFER_SIZE 10000
+
+void LoadFile(const char* fileName) {
+	//"data.txt" 파일을 열어주고 r (읽기) 모드로 설정합니다.
+	FILE* file = fopen(fileName, "r");
+
+	// 상대 경로
+	// 절대 경로
+
+	char buffer[BUFFER_SIZE] = { 0, };
+
+	fread(buffer, 1, BUFFER_SIZE, file); // 전체 파일을 읽어주는 함수
+
+	printf("%s", buffer);
+
+	fclose(file);
 }
 
-void UpdateUI()
+int main() 
 {
-	printf("\n컴퓨터가 가지고 있는 값 : ");
-}
+#pragma region 파일 쓰기
+	////"data.txt" 파일을 생성하고 w(쓰기) 모드로 설정합니다.
+	//FILE * file = fopen("data.txt","w");
 
-int main()
-{
-#pragma region rand 함수
-	// 0 ~ 32767 사이의 난수 값을 생성하고 반환하는 함수입니다.
+	//fputs("level 10", file);
 
-	//seed 값으로 현재 시간을 넣으면 초당 seed값이 계속해서 변화되기 때문에 
-	//실행할 때마다 랜덤한 난수의 값을 얻을 수 있습니다.
-	/*srand(time(NULL));
-
-	int seed = 0;
-
-
-
-	for (int i = 0; i < 5; i++)
-	{
-	seed = rand();
-
-	printf("seed의 값 : %d\n", seed % 10 + 1);
-	
-	}*/
-
-	
-#pragma endregion
-
-#pragma region 업다운 game
-	//scanf_s(); 입력
-	//크다면 컴퓨터가 가지고 있는 값보다 큽니다.
-	//작다면 컴퓨터가 가지고 있는 값보다 작습니다.
-	//Life가 0보다 작거나 같다면 게임종료.
-	//맞췄으면 Victory
-	//Life 0이면 Defeat
-	//게임 전체를 While문으로 끝났으면 break
-	srand(time(NULL));
-	int life = 5;
-	// 1. 컴퓨터 변수에 0~50사이의 난수 값을 넣어줍니다.
-	int computer = rand() % 50 + 1;
-	// 2. 컴퓨터가 가지고 있는 값을 맞추기 위한 변수 선언
-	int answer = 0;
-
-
-	while (1)
-	{
-		UpdateHPBar(life); // HPBar를 출력하는 함수
-		UpdateUI(); // 입력을 받을 수 있는 텍스트를 입력하는 함수
-		// 컴퓨터가 가지고 있는 값을 입력합니다.
-		scanf_s("%d", &answer);
-
-		system("cls");
-
-		if (answer == computer)
-		{
-			printf("\n V i c t o r y");
-			break;
-		}
-		else if (answer < computer)
-		{
-			life--;
-			printf("컴퓨터가 가지고 있는 값보다 작습니다.");
-		}
-		else if (answer > computer)
-		{
-			life--;
-			printf("컴퓨터가 가지고 있는 값보다 큽니다.");
-		}
-
-		if (life <= 0)
-		{
-			printf("\nDefeat");
-			break;
-		}
-	}
-		
-		
-		
-		
-		
-	/*	for (int i = 0; i < life; i++)
-	{
-	printf("♥");
-	}
-	printf("\n");
-	system("cls");
-	}*/
-	
+	//// 파일을 닫아줍니다.
+	//fclose(file);
 
 #pragma endregion
+#pragma region 파일 이어쓰기
+	////"data.txt" 파일을 생성하고 a (이어쓰기) 모드로 설정합니다.
+	//FILE * file = fopen("data.txt","a");
+
+	//fputs("\nMP 100", file);
+
+	//// 파일을 닫아줍니다.
+	//fclose(file);
+#pragma endregion
+#pragma region 파일 읽기
+	////"data.txt" 파일을 열어주고 r (읽기) 모드로 설정합니다.
+	//FILE * file = fopen("Image/Pika.txt","r");
+
+	//// 상대 경로
+	//// 절대 경로
+
+	//char buffer[BUFFER_SIZE] = { 0,};
+
+	//fread(buffer, 1, BUFFER_SIZE, file); // 전체 파일을 읽어주는 함수
+
+	//printf("%s", buffer);
+
+	//fclose(file);
+	
+	//함수로 txt명만 변경하면 실행될 수 있게끔
+	LoadFile("Image/Pika.txt");
+	LoadFile("Image/Meta.txt");
+
+
+#pragma endregion
+
+
 
 
 
 	return 0;
 }
+
